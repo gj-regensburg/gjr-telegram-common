b4db9609c11680ea7537f9004cbf56b69f6b5214
Benjamin Huth <Benjamin-Huth@t-online.de>
2022-03-17 00:15:05 +0100

    restructured to installable package

---
+++ gjrbotlib/__init__.py
@@ -0,0 +1,7 @@
+"""
+gjrbotlib
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Benjamin Huth'
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='gjrbotlib',
+    version='0.1.0',    
+    description='Common files for the GJR telegram bots',
+    url='https://github.com/gj-regensburg/gjr-telegram-common',
+    author='Benjamin Huth',
+    author_email='benjamin.huth@gj-regensburg.de',
+    license='MIT License',
+    packages=['gjrbotlib'],
+    install_requires=['python-telegram-bot',
+                      'arrow',                     
+                      'ics'],
+)
