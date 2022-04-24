dd1d7443ba990f15d50e6e1bc4124f05893d0421
Benjamin Huth <Benjamin-Huth@t-online.de>
2022-04-24 17:35:06 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gjrbotlib',
-    version='0.2.0',    
+    version='0.2.1',    
     description='Common files for the GJR telegram bots',
     url='https://github.com/gj-regensburg/gjr-telegram-common',
     author='Benjamin Huth',
